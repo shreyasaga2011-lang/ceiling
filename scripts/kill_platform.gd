25d92c267f5f89803b08526ea748d3bf920bd542
shreyasaga2011-lang <shreyasaga2011@gmail.com>
2026-03-22 12:19:31 -0500

    styff

--- scripts/kill_platform.gd
+++ scripts/kill_platform.gd
@@ -1,4 +1,5 @@
 extends Area2D
+@onready var cpu_particles_2d_2: CPUParticles2D = $CPUParticles2D2
 
 
 # Called when the node enters the scene tree for the first time.
@@ -11,5 +12,7 @@ func _process(delta: float) -> void:
 	pass
 
 
-func _on_body_entered(body: CharacterBody2D):
+func _on_body_entered(body: Node2D) -> void:
+	cpu_particles_2d_2.emitting = true
+	await get_tree().create_timer(0.4).timeout
 	get_tree().reload_current_scene()
